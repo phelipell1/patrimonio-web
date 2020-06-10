f377612e4203baa08b21b9bbfce8842bbca0a8dc
Phelype Rodrigo <phelipell1@gmail.com>
2020-06-10 13:12:15 -0300

    commit

--- views/coletor.php
+++ views/coletor.php
@@ -3,7 +3,7 @@ require_once('../imports_stilos/imports_stiles.php');
 ?>
 <script>
     //chamada responsavel pela leitura da pagina. retira-lo impedira que o script funcione
-    $(document).ready(function(){
+    $(document).ready(function() {
 
         //Parte responsavel por setar a configuração inicial da página.
 
@@ -15,213 +15,159 @@ require_once('../imports_stilos/imports_stiles.php');
         $("#cbmMarca").attr("disabled", true);
         $("#cbmModelo").attr("disabled", true);
         $("#textDataCompra").prop("readonly", true);
-        
+
 
     });
 </script>
-<!--<div class="container">-->
-<div class="shadow-lg mb-5 p-3 rounded border">
-    <div class="col-sm-12">
-        <form id="">
-            <div class="row">
-
-                <div class="form-group">
-                    <label for="cbmEstado">Estado:</label>
-                    <select name="cbmEstado" id="cbmEstado" class="input_form">
-                        <option selected>--</option>
-                        <option value="">Grande demais</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="cbmRegiao">Região:</label>
-                    <select name="cbmRegiao" id="cbmRegiao" class="input_form">
-                        <option selected>--</option>
-                        <option value="">Palavra grande</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="cbmCidade">Cidade:</label>
-                    <select name="cbmCidade" id="cbmCidade" class="input_form cbm_cidade">
-                        <option selected>--</option>
-                        <option value="">Palavra grande</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="cbmCidade">Departamento:</label>
-                    <select name="cbmCidade" id="cbmCidade" class="input_form">
-                        <option selected>--</option>
-                        <option value="">Palavra grande</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <input type="text" name="txtBusca" id="txtbusca" class="input_tamanho">
-                    <button type="button">buscar</button>
-                </div>
-
-            </div>
-        </form>
-
-    </div>
-    <div class="row">
-        <div class="col-sm-7 color">
-            <table class="table table-striped table-sm border">
-                <thead>
-                    <tr class="tr-tamanhos">
-                        <th>Departamento</th>
-                        <th>N° Coletor</th>
-                        <th>Serial/IMEI</th>
-                        <th>Patrimônio</th>
-                        <th>Modelo</th>
-                        <th>MicroSD</th>
-                        <th>Status</th>
-                        <th>Emprestado em</th>
-                    </tr>
-                </thead>
-                <tbody class="">
-                    <tr class="tr-corpo-tabela">
-                        <th>Leitura</th>
-                        <td>001</td>
-                        <td>354130108210599</td>
-                        <td>07575</td>
-                        <td>MOTO G7 Play</td>
-                        <td><input type="checkbox"></td>
-                        <td>Ativo</td>
-                        <td>06/06/2020</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-
-        <div class="col-sm-3">
+<div class="container">
+    <div class="shadow-lg mb-5 p-5 rounded border">
+        <div class="col-sm-12">
             <form id="">
-                <div class="form-group">
-                    <label for="textCodColetor">Código Coletor: </label>
-                    <input type="text" name="textCodColetor" id="textCodColetor" class="col-sm-3 float-right" readonly>
-                </div>
+                <div class="row">
 
-                <div class="form-group">
-                    <label for="cbmMarca">Marca: </label>
-                    <select type="text" name="cbmMarca" id="cbmMarca" class="col-sm-8 float-right" aria-readonly="true">
-                        <option selected>--</option>
-                        <option value="">Bixolon</option>
-                        <option value="">Motorola</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="cbmModelo">Modelo: </label>
-                    <select type="text" name="cbmModelo" id="cbmModelo" class="col-sm-8 float-right">
-                        <option selected>--</option>
-                        <option value="">Modelo 1</option>
-                        <option value="">Modelo 2</option>
-                    </select>
-                </div>
-
-                <div class="form-group">
-                    <label for="textDataCompra">Dt compra: </label>
-                    <input type="text" name="textDataCompra" id="textDataCompra" class="col-sm-8 float-right" readonly>
-                </div>
-
-                <div class="form-group">
-                    <label for="textDataSucata">Dt sucata: </label>
-                    <input type="date" name="textDataSucata" id="textDataSucata" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textColReserva">Col reserva: </label>
-                    <input type="text" name="textColReserva" id="textColReserva" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textNSerie">N° Série: </label>
-                    <input type="text" name="textNSerie" id="textNSerie" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textSerialMLB">Serial MLB: </label>
-                    <input type="text" name="textSerialMLB" id="textSerialMLB" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textNBateria">N° Bateria: </label>
-                    <input type="text" name="textNBateria" id="textNBateria" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textNPatrimonio">N° patrimônio: </label>
-                    <input type="text" name="textNPatrimonio" id="textNPatrimonio" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textNColetor">N° Coletor: </label>
-                    <input type="text" name="textNColetor" id="textNColetor" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textLacre">Lacre: </label>
-                    <input type="text" name="textLacre" id="textLacre" class=" col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textBootCode">Boot code: </label>
-                    <input type="text" name="textBootCode" id="textBootCode" class=" col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textOsCode">OS code: </label>
-                    <input type="text" name="textOsCode" id="textOsCode" class="col-sm-8  float-right">
-                </div>
-
-                <div class="form-group">
-                    <label for="textobservacao">Observação </label>
-                    <input type="text" name="textobservacao" id="textobservacao" class="col-sm-8 float-right">
-                </div>
-
-                <div class="form-group">
-                    <button type="button" id="btn_imprimir">Imprimir</button>
-                    <button type="button" id="btn_editar">Editar</button>
-                    <button type="button" id="btn_cancelar">Cancelar</button>
-                    <button type="button" id="btn_novaos">Nova OS</button>
-                    <button type="button" id="btn_historico">Histórico</button>
-                    <button type="button" id="btn_sair">Sair</button>
-
-                </div>
-        </div>
-
-        <div class="col-sm-2">
-            <div class="row">
-                <fieldset class="fieldset1 border">
-                    <legend>Status</legend>
                     <div class="form-group">
-                        <label for="cbmStatus">Status: </label><br>
-                        <select type="text" name="cbmModelo" id="cbmModelo" class="col-sm-12">
-                            <option selected>--</option>
-                            <option value="">Modelo 1</option>
-                            <option value="">Modelo 2</option>
+                        <select name="cbmEstado" id="cbmEstado" class="form-control form-control-sm">
+                            <option selected>Estado</option>
                         </select>
                     </div>
-                </fieldset>
 
-                <fieldset class="fieldset1 border">
-                    <legend>CHIP GPRS</legend>
-                    <div class="form-group">
-                        <label for="textobservacao">IccID</label><br>
-                        <input type="text" name="textobservacao" id="textobservacao" class="col-sm-10">
-                    </div>
-                    <div class="form-group">
-                        <label for="textobservacao">Número</label><br>
-                        <input type="text" name="textobservacao" id="textobservacao" class="col-sm-8">
+                    <div class="form-group col-sm-2">
+                        <select name="cbmRegiao" id="cbmRegiao" class="form-control form-control-sm">
+                            <option selected>Região</option>
+                        </select>
                     </div>
 
-                </fieldset>
+                    <div class="form-group col-sm-3">
+                        <select name="cbmCidade" id="cbmCidade" class="form-control form-control-sm">
+                            <option selected>Cidade</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group col-sm-2">
+                        <select name="cbmDepartamento" id="cbmDepartamento" class="form-control form-control-sm">
+                            <option selected>Departamento</option>
+                        </select>
+                    </div>
+                    <div class="col-sm-1"></div>
+                    <div class="col-sm-2">
+                        <input type="text" name="textBusca" id="textBusca" placeholder="00000" class="form-control form-control-sm">
+                    </div>
+                    <div>
+                        <button type="button" class="btn btn-sm btn-primary">buscar</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="row">
+            <div class="col-sm-7">
+                <table class="table tabela">
+                    <thead>
+                        <tr class="tr-tamanhos">
+                            <th>Departamento</th>
+                            <th>N° Coletor</th>
+                            <th>N° Serial/IMEI</th>
+                            <th>Patrimônio</th>
+                            <th>Modelo</th>
+                            <th>MicroSD</th>
+                            <th>Status</th>
+                            <th>Emprestado</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="tr-corpo-tabela">
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                    </tbody>
+
+                </table>
+                <p>Qtde total</p>
             </div>
+            <div class="col-sm-5 border">
+                <form id="">
+                    <div class="row">
+                        <div class="col-sm-6 border">
+                            <div class="form-group">
+                                <input type="text" name="textCodColetor" id="textCodColetor" class="form-control form-control-sm" placeholder="cód coletor">
+                            </div>
+                            <div class="form-group">
+                                <select name="cbmMarca" id="cbmMarca" class="form-control form-control-sm">
+                                    <option selected>Marca</option>
+                                </select>
+                            </div>
 
+                            <div class="form-group">
+                                <select name="cbmModelo" id="cbmModelo" class="form-control form-control-sm">
+                                    <option selected>Modelo</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textDataCompra" id="textDataCompra" class="form-control form-control-sm" placeholder="Data Compra">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textDataSucata" id="textDataSucata" class="form-control form-control-sm" placeholder="Data Sucata">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textDataEmprestimo" id="textDataEmprestimo" class="form-control form-control-sm" placeholder="Data Emprestimo">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textColReserva" id="textColReserva" class="form-control form-control-sm" placeholder="Col Resersa">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textNSerie" id="textNSerie" class="form-control form-control-sm" placeholder="N° Série">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textSerialMlb" id="textSerialMlb" class="form-control form-control-sm" placeholder="Serial MLB">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textNdaBateria" id="textNdaBateria" class="form-control form-control-sm" placeholder="N° da Bateria">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textNPatrimonio" id="textNPatrimonio" class="form-control form-control-sm" placeholder="N° Patrimônio">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textNdoColetor" id="textNdoColetor" class="form-control form-control-sm" placeholder="N° do Coletor">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textLacre" id="textLacre" class="form-control form-control-sm" placeholder="N° do Lacre">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textBootCode" id="textBootCode" class="form-control form-control-sm" placeholder="Boot code">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textOsCode" id="textOsCode" class="form-control form-control-sm" placeholder="Os code">
+                            </div>
+
+                            <div class="form-group">
+                                <input type="text" name="textObservacao" id="textObservacao" class="form-control form-control-sm" placeholder="Observação">
+                            </div>
+                        </div>
+                        <div class="col-sm-6 border">
+                            <Fieldset class="fieldset">
+                                <legend>Teste</legend>
+                            </Fieldset>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-    </form>
 </div>
-<!--</div>-->
